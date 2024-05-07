5a54c92e9fdb80d21bbcc5b17c7a7f6ab4231d41
CaspyCatNHA <10503103@ucn.dk>
2024-05-07 10:15:20 +0200

    ShiftDAO, DB, Shift
    added shift DAO pattern

---
+++ src/model/Shift.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Shift {
+
+	private String type;
+	private float overtimeSalary;
+	private Patient patient;
+	public Shift(String type, float overtimeSalary, Patient patient) {
+		this.type = type;
+		this.overtimeSalary = overtimeSalary;
+		this.patient = patient;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setOvertimeSalary(float overtimeSalary) {
+		this.overtimeSalary = overtimeSalary;
+	}
+	
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	} 
+	
+	
+}
